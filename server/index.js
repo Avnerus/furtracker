dcd81ac537573d84db155da07d538470b21713a5
Avnerus <avnerus@gmail.com>
2017-07-07 19:30:10 +0900

    Runnign NUIMotion on Node with Kinect2

--- server/index.js
+++ server/index.js
@@ -2,12 +2,44 @@ import express from 'express';
 import socketio from 'socket.io'
 import _ from 'lodash';
 
+import nuimotion from 'nuimotion';
+
 const app = express();
 const server = require('http').Server(app);
 const io = socketio(server);
 
 app.use(express.static('public'));
 
+
+nuimotion.startSkeletonListener( [
+    nuimotion.Joints.LEFT_HAND,
+    nuimotion.Joints.RIGHT_HAND ],
+    onSkeletonUpdate /* , 50 (the default) */ );
+
+nuimotion.addListener(
+    [ nuimotion.Events.SKELETON_TRACKING,
+        nuimotion.Events.SKELETON_STOPPED_TRACKING ],
+onEvent );
+
 server.listen(3000, function () {
       console.log('listening on port 3000!');
 });
+
+nuimotion.init();
+
+process.on('exit', function() {
+    nuimotion.close();
+});
+
+
+function onSkeletonUpdate(skeleton) {
+    console.log(skeleton);
+}
+
+function onEvent(event) {
+    if (event.eventType == nuimotion.Events.GESTURE) {
+        console.log("Gesture: " + event.gestureType + " Hand: " + event.hand + " State: " + event.step);
+    } else {
+        console.log("Event: " + event.eventType);
+    }
+}
